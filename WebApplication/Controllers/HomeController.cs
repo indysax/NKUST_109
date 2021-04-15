dd395395d2c221ca323db9d43db0cff1a9098963
YD_360 <a4836659@gmail.com>
2021-04-15 10:06:44 +0800

    Web view

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -27,6 +27,7 @@ namespace WebApplication.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ WebApplication/Controllers/MyDataController.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Controllers
+{
+    public class MyDataController : Controller
+    {
+        
+
+        public IActionResult MyData()
+        {
+            var service = new ConsoleApp.Services.ImportJsonService();
+            var filePath = ConsoleApp.Utils.FilePath.GetFullPath("高雄活動.txt");
+            List<Activity> datas = service.LoadFormFile(filePath);
+            return View(datas);
+        }
+
+        
+    }
+}
